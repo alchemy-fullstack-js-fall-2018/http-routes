6fbdecb916516cacbb2fb19b022780577fb54701
KLPainter <KLPainter@gmail.com>
2018-10-04 12:22:07 -0700

    blah

--- test/app.test.js
+++ test/app.test.js
@@ -1,23 +1,53 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const Menagerie = require('../lib/models/Menagerie');
 
-// describe('hello world', () => {
-//     it('returns hello world', () => {
-//         return request(app).get('/').then(res => {
-//             expect(res.text).toEqual('hello world');
-//         });
-//     });
-// });
+const createCreature = (creature) => {
+    return request(app).post('/creatures')
+        .send(creature);
+};
 
-describe('save', () => {
-    it('saves a record', () => {
-        return request(app).post('/creature')
-            .send({ type: 'unicorn', isMagical: true })
-            .then(resultOfPost => {
-                const { id } = JSON.parse(resultOfPost);
-                expect(id).toContain(expect.any(String));
-            });
+let savedCreatures = [];
+
+beforeEach(() => {
+    Menagerie.drop();
+});
+
+beforeEach(() => {
+    const creatureObjects = [
+        { 'type':'unicorn', 'isMagical': true },
+        { 'type':'hippogriff', 'isMagical': true },
+        { 'type':'sphynx', 'isMagical': true }
+    ];
+
+    const creaturePromises = creatureObjects.map(createCreature);
+
+    return Promise.all(creaturePromises).then(resultsOfAllCreaturePosts => {
+        savedCreatures = resultsOfAllCreaturePosts
+            .map(creatureResponse => JSON.parse(creatureResponse.text));
     });
 });
+
+describe('CRUD functions', () => {
+
+    it('gets a record by id', () => {
+        return request(app).get(`/creatures/:${savedCreatures[0].id}`)
+            .then(result => {
+                const creature = JSON.parse(result.text);
+                expect(creature).toEqual(savedCreatures[0]);
+            });
+    });
+
+    it('gets all records', () => {
+        console.log ('saved creatures:', savedCreatures);
+        return request(app).get('/creatures')
+            .then(results => {
+                const array = JSON.parse(results.text);
+                expect(array.length).toEqual(3);
+            });
+    });
+
+});
+
 
 
