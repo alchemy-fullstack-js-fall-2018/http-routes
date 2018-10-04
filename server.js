b8f68cae6061f5c5b9502edba39f98c80dc342c8
Davidchhing <davidhchhing@gmail.com>
2018-10-03 17:39:02 -0700

    set up files to begin coding

---
+++ server.js
@@ -0,0 +1,13 @@
+/* eslint no-console: off */
+
+const app = require('./lib/app');
+const http = require('http');
+
+// curl 'http://localhost:7890/hi' -d '{ "name": "ryan" }'
+const server = http.createServer(app);
+
+const PORT = 7890;
+
+server.listen(PORT, () => {
+    console.log('server running on', server.address().port);
+});
