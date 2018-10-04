367dcd5f60063bd598b48e111ae02bcd3b008665
Jack <ajackofsometrades@gmail.com>
2018-10-04 11:20:05 -0700

    failing test for get requests

--- test/app.test.js
+++ test/app.test.js
@@ -4,10 +4,27 @@ const app = require('../lib/app');
 describe('job board', () => {
 
     it('get all jobs', () => {
-
         return request(app).get('/jobs').then(res => {
             expect(res.text).toEqual('[]');
         });
+    });
+
+    it('creates a job posting', () => {
+
+
+        return request(app).post('/jobs')
+            .send({ 
+                title: 'Head of Research',
+                desc: 'Top secret stuff',
+                salary: 10
+            })
+            .then(res => {
+                const json = JSON.parse(res.text);
+                expect(json.title).toEqual('Head of Research');
+                expect(json.desc).toEqual('Top secret stuff');
+                expect(json.salary).toEqual(10);
+                expect(json.id).toEqual(expect.any(String));
+            });
     });
 
 
--- test/body-parser.test.js
+++ test/body-parser.test.js
@@ -36,7 +36,6 @@ describe('body parser', () => {
         request.emit('end');
 
         return promise;
-
     });
 
 
