b9ccbf34b4ba0563ae80144c766722baee652e30
Sophie Graham <sophiergraham1@gmail.com>
2018-10-04 11:25:53 -0700

    create a tweet test passinggit add -A

--- test/app.test.js
+++ test/app.test.js
@@ -1,14 +1,16 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-
-it('create a tweet', () => {
-    return request(app).post('/tweets')
-        .send({ username: 'me', text: 'my tweet' })
-        .then(res => {
-            const json = JSON.parse(res.text);
-            expect(json.text).toEqual('my tweet');
-            expect(json.username).toEqual('me');
-            expect(json.id).toEqual(expect.any(String));
-        });
+describe('twitter clone', () => {
+    it('create a tweet', () => {
+        return request(app).post('/tweets')
+            .send({ username: 'me', text: 'my tweet' })
+            .then(res => {
+                const json = JSON.parse(res.text);
+                expect(json.text).toEqual('my tweet');
+                expect(json.username).toEqual('me');
+                expect(json.id).toEqual(expect.any(String));
+            });
+    });
 });
+
