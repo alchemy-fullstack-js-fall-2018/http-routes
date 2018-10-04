5b612d9814a90bca802be79730873ab28efeff23
Sarah Flynn <sarah.flynn@icloud.com>
2018-10-04 11:51:43 -0700

    to do post test red

--- test/app.test.js
+++ test/app.test.js
@@ -2,9 +2,20 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('to do', () => {
-    it('returns hello world', () => {
+    it('empty get returns hello world', () => {
         return request(app).get('/').then(res => {
             expect(res.text).toEqual('hello world');
         });
+    });
+
+    it('posts a to do', () => {
+        return request(app).post('/toDoList')
+            .send({ item: 'finish lab', due: '10/4' })
+            .then(res => {
+                const json = JSON.parse(res.text);
+                expect(json.item).toEqual('finish lab');
+                expect(json.due).toEqual('10/4');
+                expect(json.id).toEqual(expect.any(String));
+            });
     });
 });
