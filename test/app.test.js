41f5edcfb850f3458cd4eea76a6682a175eed102
Claire Flanagan <claireflanagan@gmail.com>
2018-10-04 11:52:12 -0700

    add test passing

--- test/app.test.js
+++ test/app.test.js
@@ -2,6 +2,22 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('ocean dwelling mammals', () => {
+    let smallWhale = { type: 'whale', size: 'small' };
+    beforeEach(() => {
+        return request(app).post('/odms')
+            .send(smallWhale)
+            .then(res => {
+                smallWhale = JSON.parse(res.text);
+            });
+    });
+    
+    it('gets all odms', () => {
+        return request(app).get('/odms').then(res => {
+            const json = JSON.parse(res.text);
+            expect(json).toEqual([smallWhale]);
+        });
+    });
+
     it('creates an ocean dwelling mammal', () => {
         return request(app).post('/odms')
             .send({ type: 'whale', size: 'large' })
@@ -14,9 +30,4 @@ describe('ocean dwelling mammals', () => {
             });
     });
 
-    // it('gets all odms', () => {
-    //     return request(app).get('/odms').then(res => {
-    //         expect(res.text).toEqual('[]');
-    //     });
-    // });
 });
