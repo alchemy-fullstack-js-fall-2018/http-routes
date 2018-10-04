03b4eb51d3ec0a67befbe85e085ee51a5ba81dd5
Davidchhing <davidhchhing@gmail.com>
2018-10-04 09:13:12 -0700

    created pet constructor and create new id and get new id constructor

--- test/app.test.js
+++ test/app.test.js
@@ -1,1 +1,9 @@
 const request = require('supertest');
+const app = require('../lib/app');
+
+describe('pets that live on a farm', () => {
+
+    it('gets a pet by id', () => {
+        return request(app).post(/pets.route)
+    });
+});
