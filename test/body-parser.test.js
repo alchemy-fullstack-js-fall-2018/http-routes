e3f8fcb881e750140d81762ee9ac6d41c6d95a81
Alex Rankin <alex.c.rankin@gmail.com>
2018-10-03 17:59:57 -0700

    first body parser test passing

---
+++ test/body-parser.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest');
+
+const bodyParser = require('../lib/body-parser');
+const http = require('http');
+
+describe('body parser', () => {
+    let request;
+    beforeEach(() => {
+        request = new http.ClientRequest();
+        request.method = 'POST';
+        request.setHeader('Content-Type', 'text/html');
+    });
+
+    it('errors if content-type is not JSON', () => {
+        request.setHeader('Content-Type', 'text/html');
+
+        const promise = bodyParser(request).catch(err => {
+            expect(err).toEqual('Only supports JSON!');
+        });
+
+        request.emit('data', '<html></html>');
+        request.emit('end');
+
+        return promise;
+    });
+});
