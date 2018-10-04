5ccc5b25c2597e60977d4752cdebb0e15284f1b3
KLPainter <KLPainter@gmail.com>
2018-10-03 18:00:00 -0700

    end of day

--- test/app.test.js
+++ test/app.test.js
@@ -1,10 +1,23 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('hello world', () => {
-    it('returns hello world', () => {
-        return request(app).get('/').then(res => {
-            expect(res.text).toEqual('hello world');
-        });
+// describe('hello world', () => {
+//     it('returns hello world', () => {
+//         return request(app).get('/').then(res => {
+//             expect(res.text).toEqual('hello world');
+//         });
+//     });
+// });
+
+describe('save', () => {
+    it('saves a record', () => {
+        return request(app).post('/creatures')
+            .send({ type: 'unicorn', isMagical: true })
+            .then(resultOfPost => {
+                const { id } = JSON.parse(resultOfPost.text);
+                expect(id).toContain(expect.any(String));
+            });
     });
 });
+
+
