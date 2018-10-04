75aebd94f51e7b3f691ab823531f581f84c7200c
Claire Flanagan <claireflanagan@gmail.com>
2018-10-04 11:55:10 -0700

    moved the tests around

--- test/app.test.js
+++ test/app.test.js
@@ -3,7 +3,9 @@ const app = require('../lib/app');
 
 describe('ocean dwelling mammals', () => {
     let smallWhale = { type: 'whale', size: 'small' };
-    beforeEach(() => {
+    let largeWhale = { type: 'whale', size: 'large' };
+    
+    beforeAll(() => {
         return request(app).post('/odms')
             .send(smallWhale)
             .then(res => {
@@ -11,23 +13,23 @@ describe('ocean dwelling mammals', () => {
             });
     });
     
-    it('gets all odms', () => {
-        return request(app).get('/odms').then(res => {
-            const json = JSON.parse(res.text);
-            expect(json).toEqual([smallWhale]);
-        });
-    });
-
     it('creates an ocean dwelling mammal', () => {
         return request(app).post('/odms')
-            .send({ type: 'whale', size: 'large' })
+            .send(largeWhale)
             .then(res => {
                 console.log('res', res.text);
                 const json = JSON.parse(res.text);
                 expect(json.size).toEqual('large');
                 expect(json.type).toEqual('whale');
                 expect(json.id).toEqual(expect.any(String));
+                largeWhale = json;
             });
     });
-
+    
+    it('gets all odms', () => {
+        return request(app).get('/odms').then(res => {
+            const json = JSON.parse(res.text);
+            expect(json).toEqual([smallWhale, largeWhale]);
+        });
+    });
 });
