6a1c95670f3d348ad5b6339031695fef80ba29b0
Sophie Graham <sophiergraham1@gmail.com>
2018-10-04 11:34:01 -0700

    wrote GET test

--- test/app.test.js
+++ test/app.test.js
@@ -12,5 +12,20 @@ describe('twitter clone', () => {
                 expect(json.id).toEqual(expect.any(String));
             });
     });
+    it('get a tweet by id', () => {
+        return request(app).post('/tweets')
+            .send({ username: 'me', text: 'my tweet' })
+            .then(createRes => {
+                const { id } = JSON.parse(createRes.text);
+                return request(app).get(`/tweets/${id}`);
+            })
+            .then(getRes => {
+                const tweet = JSON.parse(getRes.text);
+                expect(tweet.id).toEqual(expect.any(String));
+                expect(tweet.username).toEqual(expect.any(String));
+                expect(tweet.text).toEqual(expect.any(String));
+            });
+    });
 });
+
 
