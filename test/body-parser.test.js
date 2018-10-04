a61ee8a74a0e9e79874e2cdf14a3b90cf28ca2cc
Jack <ajackofsometrades@gmail.com>
2018-10-03 17:42:37 -0700

    initial body parser test

---
+++ test/body-parser.test.js
@@ -0,0 +1,26 @@
+const bodyParser = require('../lib/body-parser');
+const http = require('http');
+
+describe('body parser', () => {
+    let request;
+    beforeEach(() => {
+        request = new http.ClientRequest();
+        request.method = 'POST';
+        request.setHeader('Content-Type', 'text/html');
+    });
+
+    it('errors if content-type is not json', () => {
+        request.setHeader('Content-Type', 'text/html');
+
+        const promise = bodyParser(request).catch(err => {
+            expect(err).toEqual('Only supports JSON');
+        });
+
+        request.emit('data', '<html></html>');
+        request.emit('end');
+
+        return promise;
+    });
+
+
+});
