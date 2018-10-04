06f33a5a541892fb60e7030aa1919f755065013a
Ryan Luras <ryanluras@gmail.com>
2018-10-03 23:21:45 -0700

    changing port, adding 404 tests

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 
 const server = http.createServer(app);
 
-const PORT = 5678;
+const PORT = 8264;
 
 server.listen(PORT, () => {
     console.log('server running on', server.address().port);
--- test/app.test.js
+++ test/app.test.js
@@ -13,4 +13,20 @@ describe('Mocking a CRUD Blog Post API', () => {
                 expect(json.id).toEqual(expect.any(String));
             });
     });
+
+    it('returns 404 when there is no method', () => {
+        return request(app)
+            .patch('/posts')
+            .send({})
+            .then(res => {
+                expect(res.statusCode).toEqual(404);
+            });
+    });
+
+    it('returns 404 when there is no route', () => {
+        return request(app).get('/fakeroute').then(res => {
+            expect(res.statusCode).toEqual(404);
+        });
+    });
+
 });
