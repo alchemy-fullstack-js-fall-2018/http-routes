38783bd632f42680293bf7730a8acc8c6767d50c
Sarah Flynn <sarah.flynn@icloud.com>
2018-10-05 17:40:13 -0700

    fixed travis error

--- test/body-parser.test.js
+++ test/body-parser.test.js
@@ -1,29 +1,28 @@
 const bodyParser = require('../lib/body-parser');
-const http = require('http');
+const EventEmitter = require('events');
 
 describe('body parser', () => {
     let request;
     beforeEach(() => {
-        request = new http.ClientRequest();
+        request = new EventEmitter;
         request.method = 'POST';
-        request.setHeader('Content-Type', 'text/html');
     });
-
+    
     it('errors if content-type is not json', () => {
-        request.setHeader('Content-Type', 'text/html');
+        request.headers = { 'content-type': 'text/html' };
 
         const promise = bodyParser(request).catch(err => {
             expect(err).toEqual('Only supports JSON');
         });
-
+        
         request.emit('data', '<html></html>');
         request.emit('end');
-
+        
         return promise;
     });
-
+    
     it('parses a json request', () => {
-        request.setHeader('Content-Type', 'application/json');
+        request.headers = { 'content-type': 'application/json' };
 
         const promise = bodyParser(request).then(body => {
             expect(body).toEqual({ name: 'ryan' });
